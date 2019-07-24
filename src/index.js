8b08eb35095696e5d25047f821a0b1b08784920a
Monika Filipowska <>
2019-07-24 12:03:52 +0200

    form

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const port = process.env.PORT || 3000;
 const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, '../public');
+
 app.use(express.static(publicDirectory));
 
-io.on('connection', () => {
-	console.log('hello');
+let message = 'Hello';
+
+io.on('connection', (socket) => {
+	socket.emit('message', message);
+
+	socket.on('sendMessage', (message) => {
+		io.emit('message', message);
+	});
 });
 
 server.listen(port, () => {
