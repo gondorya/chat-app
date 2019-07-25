2106a0c632be3f21d2f7ba2f8171a851a4f17caf
Monika Filipowska <>
2019-07-25 14:04:24 +0200

    render messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,51 @@
 const socket = io();
 
+// elements
+const $messageForm = document.querySelector('#messageForm');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $locationButton = document.querySelector('#location');
+const $messages = document.querySelector('#messages');
+
+//templates
+const messageTemplate = document.querySelector('#messageTemplate').innerHTML;
+
 socket.on('message', (message) => {
-	console.log(message);
+	const html = Mustache.render(messageTemplate, { message });
+	$messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#messageForm').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+
+	$messageFormButton.setAttribute('disabled', 'disabled');
+
 	const message = e.target.elements.message.value;
-	socket.emit('sendMessage', message);
+	socket.emit('sendMessage', message, (error) => {
+		$messageFormButton.removeAttribute('disabled');
+		$messageFormInput.value = '';
+		$messageFormInput.focus();
+
+		if (error) {
+			return console.log(error);
+		}
+		console.log('delivered');
+	});
+});
+
+$locationButton.addEventListener('click', () => {
+	$locationButton.setAttribute('disabled', 'disabled');
+
+	if (!navigator.geolocation) {
+		return alert('Geolocation is not supported by your browser');
+	}
+
+	navigator.geolocation.getCurrentPosition((position) => {
+		const longitude = position.coords.longitude;
+		const latitude = position.coords.latitude;
+		socket.emit('sendLocation', { longitude, latitude }, () => {
+			$locationButton.removeAttribute('disabled');
+			console.log('Location shared');
+		});
+	});
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,8 +19,21 @@ io.on('connection', (socket) => {
 	socket.emit('message', message);
 	socket.broadcast.emit('message', 'Say hello to new user');
 
-	socket.on('sendMessage', (message) => {
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback('Profanity is not allowed');
+		}
+
 		io.emit('message', message);
+		callback('Delivered!');
+	});
+
+	socket.on('sendLocation', (location, callback) => {
+		const { longitude, latitude } = location;
+		io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
+		callback();
 	});
 
 	socket.on('disconnect', () => {
